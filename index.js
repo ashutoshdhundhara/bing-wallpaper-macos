de0e8a7137144fdce8a9252b728e83528ef126a8
Ashutosh Dhundhara <ashutoshdhundhara@yahoo.com>
2021-11-24 23:46:48 +0530

    lots of changes

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const os = require('os');
 const path = require('path');
 const https = require('https');
 const Jimp = require('jimp');
+const url = require('url');
+const qs = require('querystring');
 
 const CURR_USER = os.userInfo().username;
 const BING_MARKET = 'en-IN';
 /* here idx=0 means today */
 const BING_JSON_URL = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mbl=1&mkt=${BING_MARKET}`;
 const BING_WALL_DIR = `/Users/${CURR_USER}/Pictures/BingWallpapers`;
-const RESOLUTION = '1920x1080';
+const RESOLUTION = 'UHD';
 
 const get_bing_json = function () {
     return new Promise(function (resolve, reject) {
-        console.log('get_bing_json: getting todays image');
         https.get(BING_JSON_URL, function (response) {
             let json = '';
             response.on('data', function (data) {
@@ -57,16 +58,17 @@ const adapt_image_meta = function (bing_json) {
 
     const todays_image = bing_json.images[0];
     const fhd_url = get_fhd_image_url(todays_image);
-    const file_name = path.basename(fhd_url);
+
+    const file_name = qs.parse(url.parse(fhd_url).query).id;
 
     const image = {
         file_name: file_name,
         url: fhd_url,
         physical_path: `${BING_WALL_DIR}/${file_name}`,
-        copyright: todays_image.copyright
+        copyright: todays_image.copyright,
+        title: todays_image.title
     };
 
-    console.log(image);
     return image;
 }
 
@@ -75,12 +77,13 @@ const download_image = function (image) {
         if (!image)
             return reject('download_image: no image meta provided');
 
-        if (fs.existsSync(image.physical_path))
-            return reject('download_image: image already there');
+        if (fs.existsSync(image.physical_path)) {
+            image.exists = true;
+            return resolve(image);
+        }
 
         const file = fs.createWriteStream(image.physical_path);
 
-        console.log(`download_image: downloading image - ${image.url}`);
         https.get(image.url, function (response) {
             response.on('data', function (chunk) {
                 file.write(chunk);
@@ -110,17 +113,13 @@ const get_text_xy_coord = function (font, text) {
 
 const add_description_to_image = function (image) {
     return new Promise((resolve, reject) => {
-        console.log('add_description_to_image: loading font');
         Jimp.loadFont(Jimp.FONT_SANS_16_BLACK)
             .then(font => {
-                console.log('add_description_to_image: loading image');
                 Jimp.read(image.physical_path)
                     .then(jimp_image => {
-                        console.log('add_description_to_image: printing text');
                         const coords = get_text_xy_coord(font, image.copyright);
                         jimp_image.print(font, coords.x, coords.y, image.copyright)
                         .write(image.physical_path, (error) => {
-                            console.log('add_description_to_image: writing file');
                             if (error)
                                 return reject(error);
                             resolve(image);
@@ -131,6 +130,7 @@ const add_description_to_image = function (image) {
 }
 
 const set_desktop_background = function (image) {
+
     // const command = `osascript -e \'tell application "Finder" to set desktop picture to POSIX file "${image.physical_path}"\'`;
     const command = `osascript -e \'tell application "System Events"
         set theDesktops to a reference to every desktop
@@ -139,7 +139,7 @@ const set_desktop_background = function (image) {
         end repeat
     end tell\'`;
     return new Promise(function (resolve, reject) {
-        console.log('set_desktop_background: setting desktop background');
+
         child_process.exec(command, function (error, stdout, stderr) {
             if (error)
                 return reject(error);
@@ -151,7 +151,7 @@ const set_desktop_background = function (image) {
 const set_login_screen_background = function (file) {
     return new Promise(function (resolve, reject) {
         const lock_screen_image_path = '/Library/Caches/com.apple.desktop.admin.png';
-        fs.copyFile(file, lock_screen_image_path, function (error) {
+        fs.copyFile(file.physical_path, lock_screen_image_path, function (error) {
             if (error)
                 return reject(error);
             return resolve(file);
@@ -161,15 +161,21 @@ const set_login_screen_background = function (file) {
 
 const handle_error = function (error) {
     console.error(error);
+    process.exit(1);
 }
 
 const main = function () {
     get_bing_json()
         .then(adapt_image_meta)
         .then(download_image)
-        .then(add_description_to_image)
+        //.then(add_description_to_image)
         .then(set_desktop_background)
         .then(set_login_screen_background)
+        .then(image => {
+            console.log(image.title);
+            console.log(image.copyright);
+            process.exit(1);
+        })
         .catch(handle_error);
 }
 
